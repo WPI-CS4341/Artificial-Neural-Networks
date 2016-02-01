444a8837182834610cb3dbd1c5d754d08b4270f8
Tyler Nickerson <nickersoft@gmail.com>
2016-02-01 12:35:53 -0500

    Training now works

--- ann.py
+++ ann.py
@@ -3,39 +3,62 @@ import numpy as np
 
 class Artificial_Neural_Network:
 
-    def __init__(self, hidden_layer_size):
-        self.hidden_layer_size = hidden_layer_size
-        self.input_weights = np.random.randn(2, self.hidden_layer_size)
-        self.output_weights = np.random.randn(self.hidden_layer_size, 1)
+    def __init__(self, input_layer_size, hidden_layer_size, output_layer_size):
+        self.input_layer_size = input_layer_size #num of node of input layer
+        self.hidden_layer_size = hidden_layer_size #num of node of hidden layers
+        self.output_layer_size = output_layer_size #num of nodes of output layers
+
+        np.random.seed(1)
+
+        #Initialize network
+        # self.layer_sizes = [] #
+        # self.layer_sizes.append(self.input_layer_size)
+
+        self.weights = [
+            2 * np.random.random((self.input_layer_size, self.hidden_layer_size)) - 1, # W1
+            2 * np.random.random((self.hidden_layer_size, self.output_layer_size)) - 1 # W2
+        ]
+
+        # #Generate layer size array
+        # for i in range(self.num_hidden_layers):
+        #     self.layer_sizes.append(self.hidden_layer_size)
+        #
+        # self.layer_sizes.append(self.output_layer_size)
 
     def sigmoid(self, z):
-        return 1 / (1 + np.exp(-1 * z))
+        return 1 / (1 + np.exp(-z))
 
-    # def sigmoidPrime(x):
-    #     return x*(1-x)
+    def sigmoid_prime(self, x):
+        return x * (1 - x)
 
     def classify(self, X):
-        print(x)
-        print(self.input_weights)
-        print(np.dot(X, self.input_weights))
-        # hidden_layer = self.sigmoid(np.dot(X, self.input_weights))
-        # output = self.sigmoid(np.dot(hidden_layer, self.output_weights))
+        hidden_layer = self.sigmoid(np.dot(X, self.weights[0]))
+        output = self.sigmoid(np.dot(hidden_layer, self.weights[1]))
         return np.round(output)
 
-    def train(self, input_set, output_set):
-        percentage = round(len(training_set) * 0.20)
-        test_set = input_set[:percentage]
-        training_set = input_set[percentage:]
+    def test(self, test_set, output_set):
+        classification = self.classify(test_set)
+        error = output_set - classification
+        percent_error = float(np.count_nonzero(error)) / float(len(test_set))
+        print("Test set has percent error of " + str(percent_error * 100) + "%")
 
-        for n in xrange(self.num_hidden_layers):
-            hidden_layer = self.sigmoid(np.dot(input_set, self.input_weights))
-            output_layer = self.sigmoid(np.dot(hidden_layer, self.output_weights))
+    def train(self, training_set, output_set):
+        overall_error = 1
+        while overall_error > 0.2:
+            input_layer  = training_set
+            hidden_layer = self.sigmoid(np.dot(input_layer, self.weights[0]))
+            output_layer = self.sigmoid(np.dot(hidden_layer, self.weights[1]))
 
-            output_layer_error = output_set - predicted_output
-            output_layer_delta = output_error * self.sigmoidPrime(output_layer)
+            output_layer_error  = output_set - output_layer
+            output_layer_delta  = output_layer_error * self.sigmoid_prime(output_layer)
 
-            hidden_layer_error = output_delta.dot(self.output_weights.T)
-            hidden_layer_delta = hidden_layer_error * self.sigmoidPrime(hidden_layer)
+            hidden_layer_error  = output_layer_delta.dot(self.weights[1].T)
+            hidden_layer_delta  = hidden_layer_error * self.sigmoid_prime(hidden_layer)
 
-            self.input_weights += input_set.T.dot(hidden_layer_delta)
-            self.output_weights += hidden_layer.T.dot(output_layer_delta)
+            self.weights[0] += input_layer.T.dot(hidden_layer_delta)
+            self.weights[1] += hidden_layer.T.dot(output_layer_delta)
+
+            total_wrong         = np.count_nonzero(output_set - np.round(output_layer))
+            total_percent_error = float(total_wrong) / float(len(output_layer))
+
+            overall_error = total_percent_error
--- main.py
+++ main.py
@@ -3,29 +3,33 @@ Authors: Yang Liu (yliu17), Tyler Nickerson (tjnickerson)
 Date: Jan 28, 2016
 """
 import sys
+import numpy as np
 from ann import Artificial_Neural_Network
+# from bpl import Back_Prop_Learner
 
 def parse_file(filename):
     """Parse the file line to nodes"""
-    x = []
-    y = []
+    examples = []
+    outputs = []
+
     try:
         with open(filename, "r") as infile:
             for line in infile:
                 line = line[:-2]
                 data = line.split(" ")
-                x.append([data[0], data[1]])
-                y.append(data[2])
-            return x, y
+                examples.append([float(data[0]), float(data[1])])
+                outputs.append([float(data[2])])
     except IOError as e:
         print(e.errno, e.strerror)
+
+    return np.array(examples), np.array(outputs)
 
 def main():
     args = sys.argv[1:]
     if len(args) > 0:
         filename = args[0]
-        hidden_layer_size = 0
-        percentage = 0
+        hidden_layer_size = 5
+        percentage = 0.20
         if len(args) > 2:
             if args[1] == "h":
                 hidden_layer_size = int(args[2])
@@ -35,10 +39,25 @@ def main():
             elif args[1] == "p":
                 percentage = float(args[2])
 
-        inputs, outputs = parse_file(filename)
-        ann = Artificial_Neural_Network(hidden_layer_size)
-        for x in inputs:
-            print(int(ann.classify(x)))
+        examples, outputs = parse_file(filename)
+
+        percentage      = int(round(len(examples) * percentage))
+        testing         = examples[:percentage]
+        testing_output  = outputs[:percentage]
+        training        = examples[percentage:]
+        training_output = outputs[percentage:]
+
+        ann = Artificial_Neural_Network(
+            len(training[0]),
+            hidden_layer_size,
+            len(training_output[0])
+        )
+
+        ann.train(training, training_output)
+        ann.classify(testing, testing_output)
+
+        # learner = Back_Prop_Learner()
+        # learner.learn(examples, ann)
 
 
 if __name__ == "__main__":

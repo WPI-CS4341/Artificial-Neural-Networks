2a71ab29484dc7ca24380856d98e238ed80f9806
Tyler Nickerson <nickersoft@gmail.com>
2016-02-01 17:33:33 -0500

    Removed unneeded files

--- ann1.py
+++
@@ -1,5 +0,0 @@
-class ArtificialNeuralNetwork:
-    def __init__(self, num_hidden_layers):
-        self.num_hidden_layers = num_hidden_layers
-
-    def sigmod(point):
--- point.py
+++
@@ -1,6 +0,0 @@
-class Point:
-    def __init__(self, x1, x2, label):
-        self.x0 = 1
-        self.x1 = x1
-        self.x2 = x2
-        self.label = label
